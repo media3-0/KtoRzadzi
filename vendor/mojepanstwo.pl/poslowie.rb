dfe5e8ac57a54ffa3d22bdfe15fa0843ca8f570b
Jakub Syty <jakub.nekro@gmail.com>
2016-01-13 15:24:14 +0100

    Typo fix

--- vendor/mojepanstwo.pl/poslowie.rb
+++ vendor/mojepanstwo.pl/poslowie.rb
@@ -2,6 +2,6 @@ require_relative 'dataset.rb'
 
 module Mojepanstwo_pl class Poslowie < Dataset
   def self.get
-    get_all 'poslowie', { limit" => 1000 }
+    get_all 'poslowie', { "limit" => 1000 }
   end
 end end
