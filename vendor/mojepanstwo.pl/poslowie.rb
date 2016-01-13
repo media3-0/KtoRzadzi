432edad6dd4745ffea7ed7efdecfedfd546468df
Jakub Syty <jakub.nekro@gmail.com>
2016-01-13 15:15:43 +0100

    Poslowie list with conditions and limit

--- vendor/mojepanstwo.pl/poslowie.rb
+++ vendor/mojepanstwo.pl/poslowie.rb
@@ -2,6 +2,6 @@ require_relative 'dataset.rb'
 
 module Mojepanstwo_pl class Poslowie < Dataset
   def self.get
-    get_all 'poslowie'
+    get_all 'poslowie', { conditions => { "poslowie.kadencja" => 8, "poslowie.mandat_wygasl" => 0 }, limit => 1000 }
   end
 end end
